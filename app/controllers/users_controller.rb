26f6435d2da069448e63bb679751f1a589e14850
George Yokoyama <george_y@outlook.com>
2022-05-23 16:38:53 -0700

    Add users EDIT and UPDATE actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: %i[ edit update ]
+
   def new
     @user = User.new
   end
@@ -15,8 +17,24 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to articles_url, notice: "Your account information has been updated." }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+      end
+    end
+  end
+
   private
-    # Only allow a list of trusted parameters through.
+    def set_user
+      @user = User.find(params[:id])
+    end
+
     def user_params
       params.require(:user).permit(:username, :email, :password)
     end
