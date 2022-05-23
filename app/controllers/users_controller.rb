9b300d28ae3726e95384a698f8b966a20e66488f
George Yokoyama <george_y@outlook.com>
2022-05-23 16:16:45 -0700

    Add users CREATE action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,22 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to articles_url, notice: "Welcome to the Basic Blog #{ @user.username }. You have successfully signed up!" }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+    # Only allow a list of trusted parameters through.
+    def user_params
+      params.require(:user).permit(:username, :email, :password)
+    end
 end
