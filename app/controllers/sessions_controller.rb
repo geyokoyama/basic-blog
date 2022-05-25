5a00745c065d20be6384a66b40e877a23d739e21
George Yokoyama <george_y@outlook.com>
2022-05-24 17:42:38 -0700

    Add session NEW and CREATE actions. Still need actual session token.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    respond_to do |format|
+      if user && user.authenticate(params[:session][:password])
+        format.html { redirect_to user, notice: "You're signed in!" }
+      else
+        flash.now[:alert] = "Could not log in. Please try again."
+        format.html { render :new, status: :unprocessable_entity, alert: "Could not log in." }
+      end
+    end
+  end
+
+  def destroy
+  end
+
+end
