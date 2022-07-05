0e1f196389f4ca2c000325bfae3942c5bf4ae781
George E Yokoyama <geyokoyama@gmail.com>
2022-07-04 19:03:05 -0700

    functional testing for category creation

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -4,9 +4,25 @@ class CategoriesController < ApplicationController
   end
 
   def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      redirect_to @category, notice: "Successfully created a new category called #{@category.name}!"
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def show
   end
+
+  private
+
+    def category_params
+      params.require(:category).permit(:name)
+    end
 
 end
